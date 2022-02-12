408b6281b9888c45454b96f9563e8b2ced39f358
theishiopian <theishiopian@gmail.com>
2022-02-11 16:37:41 -0800

    Added some more test world gen

--- Assets/Scripts/AbstractCharacter.cs
+++ Assets/Scripts/AbstractCharacter.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 
 public abstract class AbstractCharacter : MonoBehaviour
 {
+    [Header("Ground Check")] 
+    public LayerMask groundMask;
+    public float castDist = 0.15f;
     public SpriteRenderer Renderer { get; private set; }
     public Collider2D Collider { get; private set; }
     public Rigidbody2D Body { get; private set; }
@@ -12,5 +15,14 @@ public abstract class AbstractCharacter : MonoBehaviour
         Renderer = GetComponent<SpriteRenderer>();
         Collider = GetComponent<Collider2D>();
         Body = GetComponent<Rigidbody2D>();
+    }
+    
+    /**
+     * Determines whether the character is touching the ground
+     */
+    public bool IsGrounded()
+    {
+        return Physics2D.BoxCast(transform.position, ((BoxCollider2D)Collider).size, 0, Vector2.down,
+            castDist, groundMask);
     }
 }
